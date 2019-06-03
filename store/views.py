12fe3524eb183c40ddee8857aa77bdbde1b8a713
Taranjeet Singh <reachtotj@gmail.com>
2019-06-03 22:27:31 +0530

    add form.

--- store/views.py
+++ store/views.py
@@ -6,6 +6,10 @@ from .models import Book
 def download_book(request):
     template_name = 'store/book_list.html'
     books = Book.objects.all()
+    if request.method == 'GET':
+        is_download = request.GET.get('is_download', None)
+        if is_download == '1':
+            print ('Request for csv processing')
     return render(request, template_name, {
         'books': books,
     })
