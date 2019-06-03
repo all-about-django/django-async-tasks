cc833e830809b71f9aaf75df938d34c763910430
Taranjeet Singh <reachtotj@gmail.com>
2019-06-03 22:22:24 +0530

    add list view for books.

--- djlibrary/settings.py
+++ djlibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from os.path import join
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'djlibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [join(BASE_DIR, 'djlibrary', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book
+
+
+def download_book(request):
+    template_name = 'store/book_list.html'
+    books = Book.objects.all()
+    return render(request, template_name, {
+        'books': books,
+    })
